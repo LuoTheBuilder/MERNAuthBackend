dfd44cc4e852690d7d17e071defa7da719621642
LuoTheBuilder <Rawbailey@outlook.com>
2022-02-20 15:49:59 -0500

    Added frontend login and logout with Material UI templates. tbd:forgotpassword, resetpassword

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
+import LoginScreen from "./components/screens/LoginScreen";
+import RegisterScreen from "./components/screens/RegisterScreen";
+import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
+import PrivateScreen from "./components/screens/PrivateScreen";
+import PrivateRoute from "./components/routing/PrivateRoute";
+//Routing
+import Private from "./components/routing/PrivateRoute";
+
 const App = () => {
-  return <div>React App</div>;
+  return (
+    <div className="app">
+      <Switch>
+        <PrivateRoute path="/" component={PrivateScreen} exact />
+        <Route path="/login" component={LoginScreen} exact />
+        <Route path="/register" component={RegisterScreen} exact />
+        <Route path="/forgotpassword" component={ForgotPasswordScreen} exact />
+        <Route
+          path="/passwordreset/:id"
+          component={ResetPasswordScreen}
+          exact
+        />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/screens/RegisterScreen.js
@@ -0,0 +1,197 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import classes from "./RegisterScreen.module.css";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Linkstyle from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Linkstyle color="inherit" href="https://mui.com/">
+        LuoTheBuilder
+      </Linkstyle>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+const RegisterScreen = ({ history }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPass, setConfPass] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      history.push("/");
+    }
+  }, [history]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
+
+    if (password !== confPass) {
+      setPassword("");
+      setConfPass("");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+      return setError("Passwords do not match");
+    }
+
+    try {
+      const { data } = await axios.post(
+        "/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+        config
+      );
+      localStorage.setItem("authToken", data.token);
+      history.push("/");
+    } catch (error) {
+      setError(error.response.data.error);
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+    const data = new FormData(event.currentTarget);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  value={username}
+                  autoComplete="email"
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  value={email}
+                  autoComplete="email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  value={password}
+                  autoComplete="new-password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="Password"
+                  id="confirmPassword"
+                  value={confPass}
+                  autoComplete="new-password"
+                  onChange={(e) => {
+                    setConfPass(e.target.value);
+                  }}
+                />
+                {error && <span style={{ color: "red" }}>{error}</span>}
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link to="/login">
+                  <Linkstyle variant="body2">
+                    Already have an account? Sign in
+                  </Linkstyle>
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default RegisterScreen;
